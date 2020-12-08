888e5b8f5f7da6e83fb2ad7430648a56cc90a6bc
Meena Haque <meena.haque@simplybusiness.co.uk>
2020-12-08 16:57:16 +0000

    add a second enpoint

---
+++ database/powers.js
@@ -0,0 +1,28 @@
+const powers = [
+	{
+		name: "ANT-MAN",
+		power: "Ability to shrink to sub-microscopic size",
+	},
+	{
+		name: "BATWOMAN",
+		power: "Highly skilled martial artis",
+	},
+	{
+		name: "CATWOMAN",
+		power: "Skilled gymnast",
+	},
+	{
+		name: "IRON MAN",
+		power: "Proficient scientist and engineer",
+	},
+	{
+		name: "BLACK PANTHER",
+		power: "Superhuman strength, speed, reflexes",
+	},
+	{
+		name: "CAPTAIN AMERICA",
+		power: "Agility, strength, speed, endurance",
+	},
+];
+
+module.exports = powers;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const cors = require("cors"); // middleware
+const cors = require("cors");
 const superheroes = require("./database/superheros");
+const powers = require("./database/powers");
 const app = express();
 let port = process.env.PORT || 3001;
 
@@ -8,7 +9,17 @@ app.use(cors());
 
 app.get("/api/v1/superheros/", (req, res) => {
 	res.setHeader("Content-Type", "application/json");
+	res.setHeader("Server", "express");
 	res.end(JSON.stringify(superheroes));
+});
+
+// converts javascript object to JSON
+// res.end will end the response process
+// res is the object
+
+app.get("/api/v1/powers/", (req, res) => {
+	res.setHeader("Content-Type", "application/json");
+	res.end(JSON.stringify(powers));
 });
 
 app.listen(port, () => {
